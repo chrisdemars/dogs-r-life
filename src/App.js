b60b1595672c4113fed531925778e87fd28284d6
Chris DeMars <cdemars86@gmail.com>
2019-03-01 20:57:51 -0500

    Adds componentDidMoutn with axios get call.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import DogButton from './components/DogButton';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+
+  componentDidMount = () => {
+    axios.get('https://dog.ceo/api/breed/terrier-american/images/random')
+    .then(response => {
+      console.log(response.data);
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  }
+
   render() {
     return (
       <div>
