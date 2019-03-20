3165d9e43de04d38fdb0266a49e2a3875f445fbf
Chris DeMars <cdemars86@gmail.com>
2019-03-19 20:53:10 -0700

    Electron stuff

--- electron-starter.js
+++ electron-starter.js
@@ -6,10 +6,10 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({ width: 800, height: 600 })
+  win = new BrowserWindow({ width: 750, height: 750 })
 
   // and load the index.html of the app.
-  win.loadURL('http://localhost:3000/')
+  win.loadURL('http://localhost:3001/')
 
   // Open the DevTools.
   win.webContents.openDevTools()
