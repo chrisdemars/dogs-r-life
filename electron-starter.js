d728cf4ce21fcddd8805c463b0486f488a44b8e6
Chris <chris@chrisdemars.com>
2021-10-04 13:56:47 -0400

    updating electron and react code

--- electron-starter.js
+++ electron-starter.js
@@ -4,15 +4,15 @@ const { app, BrowserWindow } = require('electron')
 // be closed automatically when the JavaScript object is garbage collected.
 let win
 
-function createWindow () {
+const createWindow = () --> {
   // Create the browser window.
   win = new BrowserWindow({ width: 750, height: 750 })
 
   // and load the index.html of the app.
-  win.loadURL('http://localhost:3001/')
+  win.loadURL('http://localhost:3000/')
 
   // Open the DevTools.
-  win.webContents.openDevTools()
+  // win.webContents.openDevTools()
 
   // Emitted when the window is closed.
   win.on('closed', () => {
@@ -26,7 +26,13 @@ function createWindow () {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', function () {    
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()  
+  })
+})
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
@@ -34,14 +40,6 @@ app.on('window-all-closed', () => {
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit()
-  }
-})
-
-app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (win === null) {
-    createWindow()
   }
 })
 
--- src/App.js
+++ src/App.js
@@ -5,44 +5,43 @@ import './App.css';
 import axios from 'axios';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      image: '',
+    constructor(props) {
+        super(props);
+        this.state = {
+            image: '',
+        };
     }
-  }
 
-  componentDidMount = () => {
-    this.getDogs();
-  }
+    componentDidMount = () => {
+        this.getDogs();
+    };
 
-  getDogs() {
-    axios.get('https://dog.ceo/api/breed/terrier-american/images/random')
-    .then(response => {
-      this.setState({ image: response.data.message });
-    })
-    .catch(error => {
-      console.log(error);
-    })
-  }
+    getDogs() {
+        axios
+            .get('https://dog.ceo/api/breeds/image/random')
+            .then((response) => {
+                this.setState({ image: response.data.message });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
 
-  render() {
-    const { image } = this.state;
-    return (
-      <div>
-        <header className="header">
-          <h1>Dogs R Life</h1>
-          <p>Having a bad day? Here is a dog that can cheer you up!</p>
-        </header>
-        <DogDisplay 
-          image={ image }
-        />
-        <DogButton
-          onClick={ this.getDogs.bind(this) }
-        />
-      </div>
-    );
-  }
+    render() {
+        const { image } = this.state;
+        return (
+            <div>
+                <header className='header'>
+                    <h1>Dogs R Life</h1>
+                    <p>
+                        Having a bad day? Here is a dog that can cheer you up!
+                    </p>
+                </header>
+                <DogDisplay image={image} />
+                <DogButton onClick={this.getDogs.bind(this)} />
+            </div>
+        );
+    }
 }
 
 export default App;
